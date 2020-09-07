04d6a64698f9a24afcea05f23669ccb88e3f0c0d
haryansyah-mfr <haryansyah.mfr@gmail.com>
2020-09-07 11:03:56 +0700

    Set clickListener to submitButton
    Show Toast message if button has clicked

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editZipCode: EditText = findViewById(R.id.editZipCode)
+        val submitButton: Button = findViewById(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            if (editZipCode.text.toString().length != 5 )
+                Toast.makeText(
+                    this,
+                    "Your zip code does not match",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            else Toast.makeText(
+                this,
+                "Your zip code has successfully added",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
