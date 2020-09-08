b32191c57d173c259198334b3f1f1fc3b7d20fdd
haryansyah-mfr <haryansyah.mfr@gmail.com>
2020-09-08 07:50:49 +0700

    Create Forecast Adapter and ViewHolder class
    Create instance in MainActivity set Toast if clicked

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -5,26 +5,44 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weather.data.forecast.ForecastRepository
 
 class MainActivity : AppCompatActivity() {
+    private val forecastRepository = ForecastRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val editZipCode: EditText = findViewById(R.id.editZipCode)
         val submitButton: Button = findViewById(R.id.submitButton)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val dailyForecastAdapter = DailyForecastAdapter {
+            Toast.makeText(
+                this,
+                "Has been clicked",
+                Toast.LENGTH_SHORT).show()
+        }
 
         submitButton.setOnClickListener {
-            if (editZipCode.text.toString().length != 5 )
+            val zipCode = editZipCode.text.toString()
+
+            if (zipCode.length != 5 )
                 Toast.makeText(
                     this,
                     "Your zip code does not match",
                     Toast.LENGTH_SHORT)
                     .show()
-            else Toast.makeText(
-                this,
-                "Your zip code has successfully added",
-                Toast.LENGTH_SHORT).show()
+            else forecastRepository.loadForecast(zipCode)
         }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = dailyForecastAdapter
+
+        forecastRepository.weeklyForecast.observe(this, { forecastItems ->
+            dailyForecastAdapter.submitList(forecastItems)
+        })
     }
 }
